f3a31f464db29cc25f8ac53ad8a0a93027e88245
OyamaPlati <platioyama@gmail.com>
2021-04-21 21:55:36 +0200

    Fixed missing brackets error in class declarations

--- ConnectedComponent.cpp
+++ ConnectedComponent.cpp
@@ -3,46 +3,49 @@
 #include <algorithm>
 
 namespace pltoya001 {
+	/* Default Constructor */
 	ConnectedComponent::ConnectedComponent (int pixels, int id, std::vector<std::pair<int, int>> & coords)): pixels(pixels), id(id), coords(coords) { }
 
-	ConnectedComponent::ConnectedComponent (const ConnectedComponent & rhs): pixels(rhs.pixels), id(rhs.id), coords(rhs.coords) {
-		// Copy Constructor
-	}
+	/* Copy Constructor */
+	ConnectedComponent::ConnectedComponent (const ConnectedComponent & rhs): pixels(rhs.pixels), id(rhs.id), coords(rhs.coords) {}
 
+	/* Move Constructor */
 	ConnectedComponent::ConnectedComponent (ConnectedComponent && rhs): pixels(rhs.pixels), id(rhs.id) {
- 		// Move Constructor
 		this->coords = std::move (rhs.coords);
 	}
 
+	/* Copy Assignment Operator */
 	ConnectedComponent::ConnectedComponent & operator = (const ConnectedComponent & rhs) {
 		// Guard self assignment
     		if (this == &rhs) {
         		return *this;
 		}
 
-		// assume *this manages a reusable resource
-    		if (id != rhs.id) {         // resource in *this cannot be reused
-        		                    // release resource in *this
-        	                            // preserve invariants in case next line throws
-        				    // allocate resource in *this
-    		}
+		pixels = rhs.pixels;
+		id = rhs.id;
+		coords = rhs.coords;
 
 		return *this;
 	}
 
+	/* Move Assignment Operator */
 	ConnectedComponent::ConnectedComponent & operator = (ConnectedComponent && rhs) {
 		// Guard self assignment
     		if (this == &rhs) {
         		return *this;
 		}
 
-		// Do something
+		pixels = rhs.pixels;
+		id = rhs.id;
+		this->coords = std::move (coords);
+		*this = std::move (rhs);
 
 		return *this;
 	}
 
+	/* Destructor */
 	ConnectedComponent::~ConnectedComponent () {
-		// Destructor
+		std::cout << "Destructor called. ConnectedComponent object deleted." << std::endl;
 	}
 
 	int ConnectedComponent::getPixels () { return this->pixels; }
